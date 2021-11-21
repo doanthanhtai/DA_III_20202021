4835609b629440da585f7078a408e2773c09e454
doanthanhtai <dttai1800732@student.ctuet.edu.vn>
2021-11-21 21:26:49 +0700

    Update layout item

--- app/src/main/java/com/example/tomtep/adapter/LakeAdapter.java
+++ app/src/main/java/com/example/tomtep/adapter/LakeAdapter.java
@@ -14,6 +14,7 @@ import com.example.tomtep.R;
 import com.example.tomtep.model.Ao;
 import com.example.tomtep.model.LichSuMoiTruong;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LakeAdapter extends RecyclerView.Adapter<LakeAdapter.LakeViewHolder> {
@@ -43,7 +44,10 @@ public class LakeAdapter extends RecyclerView.Adapter<LakeAdapter.LakeViewHolder
         holder.tvTenAo.setText(ao.getTenAo());
         holder.tvNgayTao.setText(ao.getNgayTao());
         holder.tvTrangThai.setText(getTrangThaiAo(ao.isTrangThai()));
-        holder.tvEnviromentDetail.setText(getChiSoMoiTruongMoiNhat(ao));
+        holder.tvChiPhi.setText(getChiPhiCuaAo(ao));
+        holder.tvPH.setText(getChiSoMoiTruongMoiNhat(ao).get(0));
+        holder.tvOxy.setText(getChiSoMoiTruongMoiNhat(ao).get(1));
+        holder.tvDoMan.setText(getChiSoMoiTruongMoiNhat(ao).get(2));
         holder.relativeLayout.setOnClickListener(v -> iClickItemLakeListener.onClick(ao));
         holder.relativeLayout.setOnLongClickListener(view -> {
             iClickItemLakeListener.onLongClick(ao);
@@ -51,13 +55,24 @@ public class LakeAdapter extends RecyclerView.Adapter<LakeAdapter.LakeViewHolder
         });
     }
 
-    private String getChiSoMoiTruongMoiNhat(Ao ao) {
+    private String getChiPhiCuaAo(Ao ao) {
+        return "120000000 đ";
+    }
+
+    private List<String> getChiSoMoiTruongMoiNhat(Ao ao) {
+        List<String> listChiSo = new ArrayList<>();
         List<LichSuMoiTruong> lichSuMoiTruongs = ao.getLichSuMoiTruongs();
         if (lichSuMoiTruongs == null) {
-            return "pH:7 Độ mặn:2/1000 oXy:3mlg";
+            listChiSo.add(0, "7");
+            listChiSo.add(1, "2 mg/l");
+            listChiSo.add(2, "2 ‰");
+            return listChiSo;
         }
         LichSuMoiTruong lichSuMoiTruong = lichSuMoiTruongs.get(lichSuMoiTruongs.size() - 1);
-        return "pH:" + lichSuMoiTruong.getpH() + " Độ mặn:" + lichSuMoiTruong.getDoMan() + " oXy:" + lichSuMoiTruong.getoXy();
+        listChiSo.add(0, String.valueOf(lichSuMoiTruong.getpH()));
+        listChiSo.add(1, lichSuMoiTruong.getoXy() + " mg/l");
+        listChiSo.add(2, lichSuMoiTruong.getDoMan() + " ‰");
+        return listChiSo;
     }
 
 
@@ -78,12 +93,8 @@ public class LakeAdapter extends RecyclerView.Adapter<LakeAdapter.LakeViewHolder
     }
 
     public static class LakeViewHolder extends RecyclerView.ViewHolder {
-        private final TextView tvMaAo;
-        private final TextView tvTenAo;
-        private final TextView tvNgayTao;
-        private final TextView tvTrangThai;
+        private final TextView tvMaAo, tvTenAo, tvNgayTao, tvTrangThai, tvChiPhi, tvDoMan, tvPH, tvOxy;
         private final RelativeLayout relativeLayout;
-        private final TextView tvEnviromentDetail;
 
         public LakeViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -92,7 +103,10 @@ public class LakeAdapter extends RecyclerView.Adapter<LakeAdapter.LakeViewHolder
             tvTenAo = itemView.findViewById(R.id.itemlake_tv_ten);
             tvNgayTao = itemView.findViewById(R.id.itemlake_tv_ngaytao);
             tvTrangThai = itemView.findViewById(R.id.itemlake_tv_trangthai);
-            tvEnviromentDetail = itemView.findViewById(R.id.itemlake_tv_enviromentdetail);
+            tvChiPhi = itemView.findViewById(R.id.itemlake_tv_chiphi);
+            tvDoMan = itemView.findViewById(R.id.itemlake_tv_doman);
+            tvPH = itemView.findViewById(R.id.itemlake_tv_ph);
+            tvOxy = itemView.findViewById(R.id.itemlake_tv_oxy);
         }
 
     }
